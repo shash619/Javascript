4eb5acab3a04728d4ab9c2f8e9c1df4480566f9e
shash619 <48689099+shash619@users.noreply.github.com>
2023-10-16 18:17:22 +0000

    changes variable added

---
+++ .devcontainer/basic/variable.js
@@ -0,0 +1,11 @@
+const accountId="4467"
+let accountName ="Rohan"
+var accountcity="kanpur"
+let accountcountry;
+// accountId="7789"
+/*
+const cannot be changed and let scope specific and function block specific
+*/
+console.log(accountId)
+
+console.table([accountId,accountName,accountcity,accountcountry])
