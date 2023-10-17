d2695b627f82f4afea3ca0e92d78860ae5785a19
shash619 <48689099+shash619@users.noreply.github.com>
2023-10-17 05:18:07 +0000

    datatypes

---
+++ .devcontainer/basic/datatypes.js
@@ -0,0 +1,18 @@
+let name="mohan"
+let age=3;
+let year;
+let islogin=true;
+let temp=null;
+//data types
+/*
+null=>standalone value
+string => ""
+number
+undefined
+boolean=>true/false
+symbol=>unique
+*/
+//object
+
+console.log(typeof temp) //null is type of object 
+console.log(typeof year)// undefined
