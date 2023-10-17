1f76454b66b6cd012f599bf24e4f58f7b3bb87f7
shash619 <48689099+shash619@users.noreply.github.com>
2023-10-17 07:07:20 +0000

    file added

---
+++ .devcontainer/basic/comparisoncheck.js
@@ -0,0 +1,11 @@
+console.log(null>0)
+console.log(null==0)// on equality check it take null as it is
+console.log(null>=0);//on cparision it convert null to 0
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>0);
+
+//=== check data type also
+console.log("2"==2)//true
+console.log("2"===2)//false
---
+++ .devcontainer/basic/datatype_coversion.js
@@ -0,0 +1,33 @@
+let score =undefined
+let number= Number(score)
+//console.log(typeof (number));
+//console.log(number)
+
+//NOTES:
+// "33"=>33
+// "33acf"=> NAN
+// null => 0
+// undefined => NAN
+// true=>1 && false=>0
+
+let isloggedin = "fff"
+let ans = Boolean(isloggedin)
+//console.log(typeof ans)
+//console.log (ans)
+
+//NOTES:
+//1=>true
+//0 => false
+//"" => false
+//"hie"=>true
+
+let newNum = 33
+let stringNum = String(newNum)
+//console.log(typeof stringNum)
+//console.log (stringNum)
+
+//********************Operations**********************
+
+console.log("2" + 3);
+console.log(2+"3"+"5");
+console.log(3+2+"6");
